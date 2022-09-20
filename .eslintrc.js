a956fc45feed09e0b1c79ce4f5aaf3da0450159d
Caleb Cox <canac@users.noreply.github.com>
2022-09-20 17:46:59 -0500

    Fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,9 @@
-module.exports = require("@blitzjs/next/eslint");
+// Hack until https://github.com/blitz-js/blitz/issues/3814 is fixed
+const config = require("@blitzjs/next/eslint");
+config.rules["react/no-unknown-property"] = [
+  2,
+  {
+    ignore: ["global", "jsx"],
+  },
+];
+module.exports = config;
